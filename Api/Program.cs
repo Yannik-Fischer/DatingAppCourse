e6dbcfffcd9bf56c4e904a25e832cda4c3583256
Yannik-Fischer <yannik.fischer@adesso.de>
2025-05-27 11:37:13 +0200

    End of Section 2

---
+++ Api/Program.cs
@@ -0,0 +1,18 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultSqlConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
