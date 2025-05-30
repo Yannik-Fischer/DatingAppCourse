807fcd3ac6e3f475d344ac9143b7a06921394e61
Yannik-Fischer <yannik.fischer@adesso.de>
2025-05-30 11:54:44 +0200

    End of Section 6: Added client routing

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -34,7 +34,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
 
         return new UserDto
         {
-            UserName = user.UserName,
+            Username = user.UserName,
             Token = tokenService.CreateToken(user)
         };
     }
@@ -63,7 +63,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
 
         return new UserDto
         {
-            UserName = user.UserName,
+            Username = user.UserName,
             Token = tokenService.CreateToken(user)
         };
     }
